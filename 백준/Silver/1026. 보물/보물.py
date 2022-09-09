941f0b3134fb6170398f046d1a7521ffd81f3afc
userkim12 <69836858+userkim12@users.noreply.github.com>
2022-09-10 01:15:32 +0900

    [Silver IV] Title: 보물, Time: 76 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,11 @@
+counter = int(input())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+sum = 0
+for i in range(counter):
+    sum += min(a) * max(b)
+    a.pop(a.index(min(a)))
+    b.pop(b.index(max(b)))
+print(sum)
