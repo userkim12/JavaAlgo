9d0df2c24d85295e8300e4da4a7a4cbbd80439b1
userkim12 <69836858+userkim12@users.noreply.github.com>
2022-09-10 14:58:53 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int x, y;
+	cin >> x >> y;
+	cout << x + y << "\n";
+	cout << x - y << "\n";
+	cout << x * y << "\n";
+	cout << x / y << "\n";
+	cout << x % y;
+}
