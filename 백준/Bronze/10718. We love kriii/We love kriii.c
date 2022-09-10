aab92586ce8ca1c2f28eda6400e635e8b64fdf2d
userkim12 <69836858+userkim12@users.noreply.github.com>
2022-09-10 21:42:51 +0900

    Update and rename We love kriii.c to We love kriii.java

--- 백준/Bronze/10718. We love kriii/We love kriii.c
+++
@@ -1,5 +0,0 @@
-int main(int argc, char *argv[]) {
-	printf("강한친구 대한육군\n");
-	printf("강한친구 대한육군");
-	return 0;
-}
---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,15 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("강한친구 대한육군");
+        bw.newLine();
+        bw.write("강한친구 대한육군");
+        bw.flush();
+        bw.close();
+    }
+}
