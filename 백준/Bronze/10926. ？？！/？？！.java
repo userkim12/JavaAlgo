6e1bec79b98adecb723c6e54b1895f3dc382921c
userkim12 <69836858+userkim12@users.noreply.github.com>
2022-09-10 14:59:45 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+		String s = sc.readLine();
+		s += "??!";
+		System.out.println(s);
+	}
+}
