cbf83e7ba67e8ba1bead41f0a1d5c378c210c17a
userkim12 <69836858+userkim12@users.noreply.github.com>
2022-12-12 21:22:27 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,12 +3,11 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        String s = Integer.toString(n);
-        for(int i=0;i<s.length();i++){
-            answer += s.charAt(i)-'0';
+        while(n != 0) {
+            answer += n%10;
+            n /= 10;
         }
         
-
         return answer;
     }
 }
