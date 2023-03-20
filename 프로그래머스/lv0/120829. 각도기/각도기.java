e9b52b47de266b3b2d856d36a22ef7e116833691
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-03-20 14:38:03 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int angle) {
+        if(angle == 90) return 2;
+        if(angle == 180) return 4;
+        if(angle < 90) return 1;
+        
+        return 3;
+    }
+}
