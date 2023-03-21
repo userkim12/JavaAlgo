6fd7adbe1c755399d31687a2d6d8d069a3683879
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-03-21 15:36:31 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,10 +1,11 @@
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[2];
+        
         for(int i: num_list) {
-            if(i%2 == 0) answer[0]++;
-            else answer[1]++;
+            answer[i%2]++;
         }
+        
         return answer;
     }
 }
