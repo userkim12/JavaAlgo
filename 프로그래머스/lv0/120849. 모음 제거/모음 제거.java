2eb1a1d2f4880f80539810f974f8176a8ba1e735
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-03-22 22:18:55 +0900

    [level 0] Title: 모음 제거, Time: 0.07 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,12 +1,12 @@
 class Solution {
     public String solution(String my_string) {
-        String[] gather = {"a", "e", "i", "o", "u"};
+        String[] vowels = {"a", "e", "i", "o", "u"};
         StringBuilder sb = new StringBuilder();
         
         for(int i=0;i<my_string.length();i++) {
             char c = my_string.charAt(i);
             for(int j=0;j<5;j++) {
-                if(gather[j].charAt(0) == c) break;
+                if(vowels[j].charAt(0) == c) break;
                 if(j >= 4) sb.append(c);
             }
         }
