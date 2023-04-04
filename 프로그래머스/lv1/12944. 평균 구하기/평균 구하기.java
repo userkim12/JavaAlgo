3b2f31a463234c891467a5e691b82337a7aaf447
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-04-04 21:13:07 +0900

    [level 1] Title: 평균 구하기, Time: 2.41 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -1,9 +1,8 @@
+import java.util.*;
+
 class Solution {
     public double solution(int[] arr) {
-        double answer = 0;
-        for(int i:arr){
-            answer += i;
-        }
-        return answer/arr.length;
+    
+        return Arrays.stream(arr).average().orElse(0);
     }
 }
