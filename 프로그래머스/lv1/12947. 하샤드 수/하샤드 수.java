02729f350854564080ca03e9409837be207f5030
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-04-05 10:47:17 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean solution(int x) {
+        boolean answer = true;
         int sum = 0;
-        String s = Integer.toString(x);
-        for(int i=0;i<s.length();i++){
-            sum += s.charAt(i) - '0';
+        String x2s = String.valueOf(x);
+        for(int i=0;i<x2s.length();i++) {
+            sum += x2s.charAt(i)-'0';
         }
-        if(x % sum == 0) return true;
-        return false;
+        return x % sum == 0;
     }
 }
