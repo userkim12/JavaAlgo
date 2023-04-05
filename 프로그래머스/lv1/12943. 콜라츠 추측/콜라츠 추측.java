3345e31ec189297e8196e9adb7e5d152bfef4aaf
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-04-05 15:18:09 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,16 +1,19 @@
 class Solution {
-    public int solution(long num) {
-        int check = 0;
-        //loop
-        while(num != 1){
-            // num %2 == 0 -> /2
-            if(num % 2 == 0) num /= 2;
-            // num %2 != 0 -> *3 + 1
-            else num = num*3+1;
-            check++;
+    public int solution(int num) {
+        int answer = 0;
+        while(num != 1) {
+            if(answer >= 500 || num < 1) return -1;
+            
+            if(num % 2 == 0) {
+                num /= 2;
+                answer++;
+            }
+            else if(num % 2 == 1) {
+                num = (num * 3) + 1;   
+                answer++;
+            }
+            
         }
-        if (check > 499) return -1;
-        return check;
-        
+        return answer;
     }
 }
