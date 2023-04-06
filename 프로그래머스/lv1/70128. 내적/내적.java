bcd499e158a2d26783d97efe767d187284a061f4
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-04-06 11:37:37 +0900

    [level 1] Title: 내적, Time: 0.05 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.java
+++ 프로그래머스/lv1/70128. 내적/내적.java
@@ -1,8 +1,9 @@
-import java.util.stream.IntStream;
-
 class Solution {
     public int solution(int[] a, int[] b) {
-        
-        return IntStream.range(0,a.length).map(n->a[n]*b[n]).sum();
+        int answer = 0;
+        for(int i=0;i<a.length;i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
     }
 }
