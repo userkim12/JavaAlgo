5c3681aecd89ee53820e7b5aec9411f86ba396a5
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-04-06 14:56:00 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean solution(String s) {
-        if(s.length() == 4 || s.length() == 6){
+        if(s.length() == 4 || s.length() == 6) {
             for(int i=0;i<s.length();i++) {
-                if(s.charAt(i) < '0' || s.charAt(i) > '9') return false;
+                if(!(Character.isDigit(s.charAt(i)))) return false;
             }
+            
             return true;
         }
-        
         return false;
     }
 }
