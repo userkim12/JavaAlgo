9c34eef646aae60d54597f7d94ade9a5fdd5000d
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-04-07 11:57:43 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.64 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -3,14 +3,13 @@ import java.util.*;
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        for(int i=0;i<n;i++){
-            String s = Integer.toBinaryString(arr1[i] | arr2[i]);
-            s = String.format("%"+n+"s", s);
-            s = s.replaceAll("1", "#");
-            s = s.replaceAll("0", " ");
-            answer[i] = s;
+        for(int i=0;i<n;i++) {
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            answer[i] = "0".repeat(n - answer[i].length()) + answer[i];
+            answer[i] = answer[i].replace("1", "#");
+            answer[i] = answer[i].replace("0", " ");
+            
         }
         return answer;
     }
-    
 }
