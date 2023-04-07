0d18fb21b63bd1c60514be1cfa9039c3fe895c8f
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-04-07 11:06:16 +0900

    [level 1] Title: 시저 암호, Time: 33.82 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,14 +1,20 @@
 class Solution {
     public String solution(String s, int n) {
         String answer = "";
-        for(char c: s.toCharArray()){
-            if(c == ' ') answer += c;
-            else if('a' <= c && c <= 'z'){
-                answer += (char)('a'+(c-'a'+n)%26);
+        for(int i=0;i<s.length();i++) {
+            char c = s.charAt(i);
+            if(Character.isLowerCase(c)){
+                char changed_c = (char) (c+n);
+                if(changed_c > 'z') changed_c = (char) (changed_c - 26);
+                answer += changed_c;
             }
-            else if(Character.isUpperCase(c)){
-                answer += (char)('A'+(c-'A'+n)%26);
+            else if(Character.isUpperCase(c)) {
+                char changed_c = (char) (c+n);
+                if(changed_c > 'Z') changed_c = (char) (changed_c - 26);
+                answer += changed_c;
             }
+            else answer += c;
+            
         }
         return answer;
     }
