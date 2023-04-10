4faaeee2d75cc7b73e33c9d6fff5e58f3a581b54
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-04-10 14:15:48 +0900

    [level 1] Title: 소수 찾기, Time: 202.17 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=2;i<=n;i++) {
+            if(isPrime(i)) answer++;
+        }
+        return answer;
+    }
+    
+    private boolean isPrime(int n) {
+        for(int i=2;i<=Math.sqrt(n);i++) {
+            if(n % i == 0) return false;
+        }
+        
+        return true;
+    }
+}
