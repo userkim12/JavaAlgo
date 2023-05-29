099caf32b4b5525cef380284aee1ce3fa03321c4
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-05-29 15:20:42 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        if(a % 2 != 0 && b % 2 != 0) answer = a*a + b*b;
+        else if((a+b) % 2 != 0) answer = 2*(a+b);
+        else answer = a > b ? a-b:b-a;
+        return answer;
+    }
+}
