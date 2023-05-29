dff9fff19e19a5798d4a49fcf5564081b719fe31
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-05-29 15:46:49 +0900

    [unrated] Title: 이어 붙인 수, Time: 1.62 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String a="", b="";
+        for(int i=0;i<num_list.length;i++) {
+            if(num_list[i] % 2 == 0) a += String.valueOf(num_list[i]);
+            else b += String.valueOf(num_list[i]);
+        }
+        
+        
+        return Integer.parseInt(a) + Integer.parseInt(b);
+    }
+}
