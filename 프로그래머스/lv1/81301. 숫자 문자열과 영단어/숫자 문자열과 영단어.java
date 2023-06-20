f5d99f18857d5bf13be1b7a351a06596322f60b6
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-06-20 20:44:44 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i=0;i<numbers.length;i++) {
             tmp = tmp.replace(numbers[i], String.valueOf(i));
         }
-        
+
         return Integer.parseInt(tmp);
     }
 }
