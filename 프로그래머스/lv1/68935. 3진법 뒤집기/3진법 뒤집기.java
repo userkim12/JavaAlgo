99bf6fb3e6eb425dc9d9ae20fec5fa9e7e590555
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-06-21 20:50:39 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,16 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        String a = "";
+        
+        // 3진법으로 변환된 수를 저장하는 변수
+        StringBuilder sb = new StringBuilder();
+        
+        // n이 3으로 나눌 필요 없을 때까지 loop
+        // 조건에 0이 아닌 1 이상의 수가 들어갈 경우 마지막 몫이 포함이 안 됨
         while(n > 0) {
-            a += Integer.toString(n%3);
+            // 3진수로 변환
+            sb.append(n%3);
             n /= 3;
         }
-
-        for(int i=0;i<a.length();i++) {
-            int d = a.charAt(i) - '0';
-            answer += d * Math.pow(3, a.length()-i-1);
+        
+        // StringBuilder에 append된 수들을 하나씩 확인할 수 있게 String으로 변환
+        String s = sb.toString();
+        
+        // 변환된 문자열을 통해 3->10진법으로 변환
+        for(int i=0;i<s.length();i++) {
+            answer += (s.charAt(i)-'0') * Math.pow(3, s.length()-i-1);
         }
+        
         return answer;
     }
 }
