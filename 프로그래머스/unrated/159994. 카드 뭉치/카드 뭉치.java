3714e02e28d5e9b2f86781c6b3ead4ed08ebd435
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-06-22 17:23:34 +0900

    [unrated] Title: 카드 뭉치, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        List<String> tmp = new ArrayList<>();
+
+        int pointer = 0;
+        int idx1 = 0, idx2 = 0;
+        int check = 0;
+
+        while (true) {
+            if (check >= goal.length) break;
+
+            if (idx1 < cards1.length && cards1[idx1].equals(goal[pointer])) {
+                tmp.add(cards1[idx1]);
+                idx1++;
+            } else if (idx2 < cards2.length && cards2[idx2].equals(goal[pointer])) {
+                tmp.add(cards2[idx2]);
+                idx2++;
+            }
+            pointer++;
+            check++;
+        }
+        
+        if(tmp.size() != goal.length) return "No";
+        
+        String answer = "Yes";
+        for (int i = 0; i < goal.length; i++) {
+            if(!(tmp.get(i).equals(goal[i]))) {
+                answer = "No";
+                break;
+            }
+        }
+        return answer;
+    }
+}
